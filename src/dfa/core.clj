ca89772ac739a648eb2960a4762a1142dd1e1953
fbmnds <fbmnds7.gmail.com>
2013-02-11 20:43:13 +0100

    restructuring tests

--- src/dfa/core.clj
+++ src/dfa/core.clj
@@ -7,32 +7,60 @@
 (defn do-transform-payload [payload-0 payload-1]
   (join payload-0 payload-1))
 
-(defn set-transform-payload [f]
+(defn set-fn-transform-payload [f]
   (def do-transform-payload f))
 
 (defn do-transform-state [[payload-0 state-0] [payload-1 state-1]]
   [(do-transform-payload payload-0 payload-1) state-1])
 
-(defn do-state-cycle [dfa x]
-  (if (empty? x)
+(defn do-cycle-states [dfa current-states]
+  (if (empty? current-states)
     nil
-    (let [[payload-0 state-0] x
-          new-states (map #(do-transform-state [payload-0 state-0] %)
-                          (state-0 (:transitions dfa)))]
-      [(map first (filter #((second %) (:accepts dfa)) new-states))
-       new-states])))
+    (apply concat
+           (map #((second %) (:transitions dfa))
+                current-states))))
 
-(defn dfa-seq [dfa]
+(defn dfa-seq- [dfa]
   (loop [current-states (list [nil (:start dfa)])
-         processed-current-states (map #(do-state-cycle dfa %) current-states)
+         processed-current-states (map #(do-cycle-states dfa %) current-states)
          result-states ()
          i 20]
     (if (< i 0)
       result-states
       (recur (reduce concat (map second processed-current-states))
-             (map #(do-state-cycle dfa %) current-states)
+             (map #(do-cycle-states dfa %) current-states)
              (set (reduce conj result-states
                           (apply concat
                                  (filter #(not (empty? %))
                                          (map first processed-current-states)))))
              (dec i)))))
+
+(defn dfa-seq [dfa]
+  (let [join
+        (fn [word letter]
+          (clojure.string/join (concat word (str letter))))
+        process-curr-state
+        (fn [[word curr-state] [letter new-state]]
+          [(join word letter) new-state])
+        process-states
+        (fn [x]
+          (if (empty? x)
+            nil
+            (let [[word state] x
+                  new-states (map #(process-curr-state [word state] %)
+                                  (state (:transitions dfa)))]
+              [(map first (filter #((second %) (:accepts dfa)) new-states))
+               new-states])))]
+    (loop [current-states (list ["" (:start dfa)])
+           processed-current-states (map process-states current-states)
+           result-states ()
+           i 20]
+      (if (< i 0)
+        result-states
+        (recur (reduce concat (map second processed-current-states))
+               (map process-states current-states)
+               (set (reduce conj result-states
+                            (apply concat
+                                   (filter #(not (empty? %))
+                                           (map first processed-current-states)))))
+               (dec i))))))
--- test/dfa/core_test.clj
+++ test/dfa/core_test.clj
@@ -2,19 +2,17 @@
   (:use clojure.test
         dfa.core))
 
-(deftest dfa-test
-  (testing "FIXME, I fail."
-    (is (= #{"a" "ab" "abc"}
-           (set (dfa-seq '{:states #{q0 q1 q2 q3}
+
+(def dfa-1 '{:states #{q0 q1 q2 q3}
                      :alphabet #{a b c}
                      :start q0
                      :accepts #{q1 q2 q3}
                      :transitions {q0 {a q1}
                                    q1 {b q2}
-                                   q2 {c q3}}}))))
+                                   q2 {c q3}}})
 
-    (is (= #{"hi" "hey" "hello"}
-           (set (dfa-seq '{:states #{q0 q1 q2 q3 q4 q5 q6 q7}
+
+(def dfa-2 '{:states #{q0 q1 q2 q3 q4 q5 q6 q7}
                      :alphabet #{e h i l o y}
                      :start q0
                      :accepts #{q2 q4 q7}
@@ -22,37 +20,35 @@
                                    q1 {i q2, e q3}
                                    q3 {l q5, y q4}
                                    q5 {l q6}
-                                   q6 {o q7}}}))))
+                                   q6 {o q7}}})
 
-    (is (= (set (let [ss "vwxyz"] (for [i ss, j ss, k ss, l ss] (str i j k l))))
-           (set (dfa-seq '{:states #{q0 q1 q2 q3 q4}
+
+(def dfa-3 '{:states #{q0 q1 q2 q3 q4}
                      :alphabet #{v w x y z}
                      :start q0
                      :accepts #{q4}
                      :transitions {q0 {v q1, w q1, x q1, y q1, z q1}
                                    q1 {v q2, w q2, x q2, y q2, z q2}
                                    q2 {v q3, w q3, x q3, y q3, z q3}
-                                   q3 {v q4, w q4, x q4, y q4, z q4}}}))))
+                                   q3 {v q4, w q4, x q4, y q4, z q4}}})
 
 
-    (is (let [res (take 2000 (dfa-seq '{:states #{q0 q1}
+(def dfa-4 '{:states #{q0 q1}
                                   :alphabet #{0 1}
                                   :start q0
                                   :accepts #{q0}
                                   :transitions {q0 {0 q0, 1 q1}
-                                                q1 {0 q1, 1 q0}}}))]
-          (and (every? (partial re-matches #"0*(?:10*10*)*") res)
-               (= res (distinct res)))))
+                                                q1 {0 q1, 1 q0}}})
 
-    (is (let [res (take 2000 (dfa-seq '{:states #{q0 q1}
+
+(def dfa-5 '{:states #{q0 q1}
                                   :alphabet #{n m}
                                   :start q0
                                   :accepts #{q1}
-                                  :transitions {q0 {n q0, m q1}}}))]
-          (and (every? (partial re-matches #"n*m") res)
-               (= res (distinct res)))))
+                                  :transitions {q0 {n q0, m q1}}})
 
-    (is (let [res (take 2000 (dfa-seq '{:states #{q0 q1 q2 q3 q4 q5 q6 q7 q8 q9}
+
+(def dfa-6 '{:states #{q0 q1 q2 q3 q4 q5 q6 q7 q8 q9}
                                   :alphabet #{i l o m p t}
                                   :start q0
                                   :accepts #{q5 q8}
@@ -64,6 +60,29 @@
                                                 q6 {o q7}
                                                 q7 {p q8}
                                                 q8 {l q9}
-                                                q9 {o q6}}}))]
+                                                q9 {o q6}}})
+
+
+(deftest dfa-test
+  (testing "FIXME, final test."
+    (is (= #{"a" "ab" "abc"}
+           (set (dfa-seq dfa-1))))
+
+    (is (= #{"hi" "hey" "hello"}
+           (set (dfa-seq dfa-2))))
+
+    (is (= (set (let [ss "vwxyz"] (for [i ss, j ss, k ss, l ss] (str i j k l))))
+           (set (dfa-seq dfa-3))))
+
+
+    (is (let [res (take 2000 (dfa-seq dfa-4))]
+          (and (every? (partial re-matches #"0*(?:10*10*)*") res)
+               (= res (distinct res)))))
+
+    (is (let [res (take 2000 (dfa-seq dfa-5))]
+          (and (every? (partial re-matches #"n*m") res)
+               (= res (distinct res)))))
+
+    (is (let [res (take 2000 (dfa-seq dfa-6))]
           (and (every? (partial re-matches #"limit|(?:loop)+") res)
                (= res (distinct res)))))))
